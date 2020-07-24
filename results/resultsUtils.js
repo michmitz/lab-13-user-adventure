aecb426d2f0e614ec2c18accd75cb34394d4abf1
Michelle Stermitz <stermitzm@gmail.com>
2020-07-24 12:03:03 -0700

    Added more to results

--- results/resultsUtils.js
+++ results/resultsUtils.js
@@ -1,8 +1,21 @@
 // import { getUser } from '../userUtils.js';
 // import { findById } from '../questUtils.js';
 
-// export function renderResults(user) {
-//     const user = getUser();
+// const death = {
+//     title: "You're dead!",
+//     description: 'You ran out of hp. Click the button below to try again!'
+// }
+
+// const victory = {
+//     title: "You won!",
+//     description: "You successfully passed all trials without dying. I know it's a low standard for winning, but some aren't as lucky as you."
+// }
+
+// const user = getUser()
+
+
+// export function renderResults(deathorvictory) {
+    
 
 //     const resultsDiv = document.getElementById('results-display');
 //     const resultP = document.createElement('p');
