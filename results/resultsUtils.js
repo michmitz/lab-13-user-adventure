2e5fa32270de98f078df4bb409912943f912d807
Michelle Stermitz <stermitzm@gmail.com>
2020-07-24 11:54:40 -0700

    Started working on results page

---
+++ results/resultsUtils.js
@@ -0,0 +1,10 @@
+// import { getUser } from '../userUtils.js';
+// import { findById } from '../questUtils.js';
+
+// export function renderResults(user) {
+//     const user = getUser();
+
+//     const resultsDiv = document.getElementById('results-display');
+//     const resultP = document.createElement('p');
+//     resultP.textContent = 
+// }
